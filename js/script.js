4d66f23d1d1912eced3d6b1f62a66ce59a5fd292
bello <bellosoliu12@gmail.com>
2022-10-12 23:46:28 +0100

    added features

--- js/script.js
+++ js/script.js
@@ -1,28 +1,48 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY >0);
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('active');
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
 
 window.onscroll = () => {
-	menu.classList.remove('bx-x');
-	navbar.classList.remove('active');
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
-const sr = ScrollReveal ({
-	distance: '25px',
-	duration: 2500,
-	reset: true
-})
+const sr = ScrollReveal({
+  distance: "25px",
+  duration: 2500,
+  reset: true,
+});
 
-sr.reveal('.home-text',{delay:190, origin:'bottom'})
+sr.reveal(".home-text", { delay: 190, origin: "bottom" });
 
-sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+sr.reveal(".about,.services,.portfolio,.contact", {
+  delay: 200,
+  origin: "bottom",
+});
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      emailjs.sendForm("service_gvj1trr", "template_k2iem65", this).then(
+        function () {
+          alert("SUCCESS!");
+        },
+        function (error) {
+          alert("FAILED...", error);
+        }
+      );
+    });
+};
